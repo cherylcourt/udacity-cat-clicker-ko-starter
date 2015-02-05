31a03441cc3388251abf24fc0949c461ac36e841
cherylcourt <cheryl.court@gmail.com>
2015-02-05 14:36:28 -0700

    Created Cat object.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-var ViewModel = function() {
+
+var Cat = function() {
     this.clickCount = ko.observable(0);
     this.name = ko.observable('Tabby');
     this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
@@ -7,10 +8,6 @@ var ViewModel = function() {
         {name: 'Hero'},
         {name: 'Cat'}
     ]);
-
-    this.incrementCounter = function() {
-        this.clickCount(this.clickCount() + 1);
-    };
 
     this.level = ko.computed(function() {
         if(this.clickCount() > 200) {
@@ -28,6 +25,14 @@ var ViewModel = function() {
         else return 'Newborn';
 
     }, this);
+};
+
+var ViewModel = function() {
+    this.currentCat = ko.observable( new Cat() );
+
+    this.incrementCounter = function() {
+        this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+    };
 };
 
 ko.applyBindings(new ViewModel());
