1dfcb457bd3d3ecab699770d7755855f342fc83d
cherylcourt <cheryl.court@gmail.com>
2015-02-05 14:28:01 -0700

    Added nicknames.

--- js/app.js
+++ js/app.js
@@ -2,6 +2,11 @@ var ViewModel = function() {
     this.clickCount = ko.observable(0);
     this.name = ko.observable('Tabby');
     this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
+    this.nicknames = ko.observableArray([
+        {name: 'Tiger'},
+        {name: 'Hero'},
+        {name: 'Cat'}
+    ]);
 
     this.incrementCounter = function() {
         this.clickCount(this.clickCount() + 1);
