f9b07dea77cd0c16c7708565b4836702a70505c8
Cheryl Court <cheryl.court@gmail.com>
2015-02-06 07:55:05 -0700

    Changed nicknames to to array of strings; modified Cat to take a data object; initialized array of simple cat objects.

--- js/app.js
+++ js/app.js
@@ -1,13 +1,46 @@
+var initialCats = [
+    {
+        clickCount: 0,
+        name: 'Tabby',
+        imgSrc: 'img/434164568_fea0ad4013_z.jpg',
+        imgAttribution: 'https://www.flickr.com/photos/bigtallguy/434164568',
+        nicknames: ['Tiger', 'Hero', 'Cat']
+    },
+    {
+        clickCount: 0,
+        name: 'Tiger',
+        imgSrc: 'img/4154543904_6e2428c421_z.jpg',
+        imgAttribution: 'https://www.flickr.com/photos/xshamx/4154543904',
+        nicknames: ['Tigger']
+    },
+    {
+        clickCount: 0,
+        name: 'Scaredy',
+        imgSrc: 'img/22252709_010df3379e_z.jpg',
+        imgAttribution: 'https://www.flickr.com/photos/kpjas/22252709',
+        nicknames: ['Casper']
+    },
+    {
+        clickCount: 0,
+        name: 'Shadow',
+        imgSrc: 'img/1413379559_412a540d29_z.jpg',
+        imgAttribution: 'https://www.flickr.com/photos/malfet/1413379559',
+        nicknames: ['Shooby']
+    },
+    {
+        clickCount: 0,
+        name: 'Sleepy',
+        imgSrc: 'img/9648464288_2516b35537_z.jpg',
+        imgAttribution: 'https://www.flickr.com/photos/onesharp/9648464288',
+        nicknames: ['Zzzzzz']
+    }
+];
 
-var Cat = function() {
-    this.clickCount = ko.observable(0);
-    this.name = ko.observable('Tabby');
-    this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-    this.nicknames = ko.observableArray([
-        {name: 'Tiger'},
-        {name: 'Hero'},
-        {name: 'Cat'}
-    ]);
+var Cat = function(data) {
+    this.clickCount = ko.observable(data.clickCount);
+    this.name = ko.observable(data.name);
+    this.imgSrc = ko.observable(data.imgSrc);
+    this.nicknames = ko.observableArray(data.nicknames);
 
     this.level = ko.computed(function() {
         if(this.clickCount() > 200) {
@@ -28,7 +61,15 @@ var Cat = function() {
 };
 
 var ViewModel = function() {
-    this.currentCat = ko.observable( new Cat() );
+    var self = this;
+
+    this.catList = ko.observableArray([]);
+
+    initialCats.forEach(function(catItem) {
+        self.catList.push(new Cat(catItem));
+    });
+
+    this.currentCat = ko.observable( this.catList()[0] );
 
     this.incrementCounter = function() {
         this.clickCount(this.clickCount() + 1);
